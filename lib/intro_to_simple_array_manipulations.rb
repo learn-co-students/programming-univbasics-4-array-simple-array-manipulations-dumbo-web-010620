f904ef7f86b29b2718b0456fa932e3a27ae8a582
wesley perez <wesley.m.perez@gmail.com>
2019-12-26 14:33:59 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 def using_push(array,string)
- games = ["mario","call of duty","wwe"]
- games.push("violet")
+ colors = ["blue","red","green"]
+ colors.push("violet")
  end
 
 def using_unshift(array,string)
@@ -9,37 +9,29 @@ def using_unshift(array,string)
 end
 
 def using_pop(array)
-  contients = ["North America", "South America", "Europe", "Africa", "Asia", "Antarctica"]
-  contients.pop
-
+array.pop
 end
 
 def pop_with_args(array)
- small_dogs = ["Chihuahua","Shiba Inu"]
- dog_breeds = [small_dogs]
-  dog_breeds.pop
+array.pop(2)
 end
 
+
 def using_shift(array)
-  my_favorite_cities = ["Lagos","jersey","bayonne","manhattan","baltimore","toms river"]
-  my_favorite_cities.shift
+  array.shift
 end
 
 def shift_with_args(array)
-  games = ["mario","call of duty",]
-  games.shift("mario","call of duty")
+array.shift(2)
+
 end
 
 def using_concat(array1,array2)
-all_my_favs = ["raindrops on roses", "whiskers on kittens", "sports cars", "flatiron school"]
-  characters = ["luigi","golem"]
-  games.concat(characters)
+array1.concat(array2)
 end
 
 def using_insert(array,element)
-  games = ["mario","call of duty","wwe","need for speed"]
-  new = "wow"
-  games.insert(new)
+array.insert(4,element)
 end
 
 def using_uniq(array)
@@ -50,14 +42,11 @@ end
 def using_flatten(array)
   characters = ["Drums","Flute"]
   games = ["Saxophone", "Piano", "Trumpet", "Violin",characters]
-  games.flatten(characters)
+  games.flatten
 end
 
 def using_delete(array,string)
-  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  instructors.include?("Steven")
-  while true do
-    instructors.delete("Steven")
+array.delete(string)
 end
 
 def using_delete_at(array,integer)
